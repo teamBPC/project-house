571da404c37685914c975bb868d54570920b022a
LAPTOP-SSV2GS54\qortl <qortlgus100@gmail.com>
2023-09-10 17:34:01 +0900

    board delete 버튼 modal제작

--- client/src/pages/projects/DeleteBoardModal.tsx
+++ client/src/pages/projects/DeleteBoardModal.tsx
@@ -114,7 +114,7 @@ function DeleteBoardModal({
               </div>
               <button
                 type="submit"
-                className="w-full text-white bg-red-700 hover:bg-red-800 focus:ring-4  focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+                className="w-full text-white bg-red-700 hover:bg-red-800 focus:ring-4  focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
               >
                 삭제하기
               </button>
