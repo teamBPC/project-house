211beb668be68fa5f3cb878fd1c73e66bfe4251f
LAPTOP-SSV2GS54\qortl <qortlgus100@gmail.com>
2023-09-10 23:40:31 +0900

    master 브랜치와 병합 충돌 해결

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-// module.exports = {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   },
-// }
-=======
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
->>>>>>> 1350edb04dfe94cde0353e792bb3df2944aa32c3
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,4 @@
 function Home() {
-<<<<<<< HEAD
-    return <></>;
-  }
-  
-  export default Home;
-  
-=======
   return <></>;
 }
 export default Home;
->>>>>>> 1350edb04dfe94cde0353e792bb3df2944aa32c3
